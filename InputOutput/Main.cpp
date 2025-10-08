7adffc6a77ff16e5f3438721460241a6b4779ed6
akinuro <akinurobusiness@gmail.com>
2025-10-09 02:16:12 +0300

    123

--- InputOutput/Main.cpp
+++ InputOutput/Main.cpp
@@ -1,4 +1,32 @@
+#include <iostream>
+#include <string>
+#include <limits>
+
 int main()
 {
+	std::string name;
+	int age;
+
+	std::cout << "What is you name?" << std::endl;
+	std::cin >> name;
+
+	std::cout << "What is you age?" << std::endl;
+	std::cin >> age;
+
+	while (std::cin.fail())
+	{
+		std::cout << "ERROR: I expected a number" << std::endl;
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cin >> age;
+	}
+	//hello git
+	std::cout << "Hello, " << name;
+	std::cout << ". You are " << age << " years old." << std::endl;
+
+	std::cout << "Press Enter to continue..." << std::endl;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	std::cin.get();
+
 	return 0;
 }
